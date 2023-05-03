df78cfdc9f32898fa0b1971a977b2736ba8647da
Scott Sickles <scottsickles@gmail.com>
2023-05-02 23:50:00 -0400

    match up to actual output

--- mysql.tf
+++ mysql.tf
@@ -5,7 +5,7 @@ data "ns_connection" "mysql" {
 }
 
 locals {
-  db_name      = data.ns_connection.mysql.outputs.db_name
+  db_name      = data.ns_connection.mysql.outputs.db_instance_name
   db_endpoint  = data.ns_connection.mysql.outputs.db_endpoint
   db_subdomain = split(":", local.db_endpoint)[0]
   db_port      = split(":", local.db_endpoint)[1]
