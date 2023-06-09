7660b8b182173745652c2e9efd9d03ec864ca59d
Brad Sickles <brad.sickles@gmail.com>
2023-06-09 10:20:52 -0400

    GCP cloud functions require an id token with a target audience

--- restapi.tf
+++ restapi.tf
@@ -1,10 +1,11 @@
 provider "google" {
   alias       = "invoker"
-  credentials = local.db_admin_invoker.private_key
+  credentials = base64decode(local.db_admin_invoker.private_key)
 }
 
-data "google_client_config" "invoker" {
-  provider = google.invoker
+data "google_service_account_id_token" "invoker" {
+  target_audience = coalesce(local.db_admin_func_url, "https://missing-db-admin-url")
+  provider        = google.invoker
 }
 
 provider "restapi" {
@@ -12,6 +13,6 @@ provider "restapi" {
   write_returns_object = true
 
   headers = {
-    "Authorization" : "Bearer ${data.google_client_config.invoker.access_token}"
+    "Authorization" : "Bearer ${data.google_service_account_id_token.invoker.id_token}"
   }
 }
